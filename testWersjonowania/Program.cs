66a5007a2dd18334d02166424f9bfab6a9d7e7e9
Stefan <stefbat666@wp.pl>
2019-04-04 12:56:37 +0200

    wersja przeniesiona do GITa

---
+++ testWersjonowania/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testWersjonowania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program ddemonstruje działanie wersjonowania w GITHUB-ie");
+            Console.WriteLine("wersja wczytana do GITHUBa");
+            Console.WriteLine("Kolejnosc 1");
+            Console.ReadLine();
+
+        }
+    }
+}
